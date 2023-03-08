fd955e378faed102e848b48dc7ca2b03706b0e0d
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2023-03-08 15:12:12 -0500

    Update States.jsx

--- Project/04/components/states/States.jsx
+++ Project/04/components/states/States.jsx
@@ -4,12 +4,12 @@ import './States.css';
 /**
  * Define States, a React component of Project 4, Problem 2. The model
  * data for this view (the state names) is available at
- * window.models.statesModel().
+ * window.models.states.
  */
 class States extends React.Component {
   constructor(props) {
     super(props);
-    console.log('window.models.statesModel()', window.models.statesModel());
+    console.log('window.models.states', window.models.states);
   }
 
   render() {
